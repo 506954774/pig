925b3c35dd1ac6f070ff536a37dc768e64048c31
ychen <991778207@qq.com>
2021-12-02 11:45:11 +0800

    :art: 小程序端接口

---
+++ pig-dc/pig-dc-biz/src/main/java/com/pig4cloud/pig/dc/biz/controller/MiniOffferController.java
@@ -0,0 +1,95 @@
+package com.pig4cloud.pig.dc.biz.controller;
+
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.pig4cloud.pig.common.core.util.R;
+import com.pig4cloud.pig.dc.api.dto.QueryPageDTO;
+import com.pig4cloud.pig.dc.api.entity.OscOffer;
+import com.pig4cloud.pig.dc.biz.service.IOscOfferService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import org.apache.http.util.TextUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * @author LeiChen
+ * @version 1.0
+ * @date 2021/5/21 12:37
+ */
+@RestController
+@AllArgsConstructor
+@RequestMapping("/mini/offer")
+@Api(value = "MiniOffferController", tags = "小程序-offer榜单")
+public class MiniOffferController {
+
+	private final IOscOfferService iOscOfferService;
+
+
+
+
+	/**
+	 *  offer榜单分页列表
+	 *
+	 * @return
+	 */
+	@ApiOperation(value = " offer榜单分页列表", notes = " offer榜单分页列表")
+	@PostMapping("/page")
+	public R page(@RequestBody @Valid QueryPageDTO dto) {
+		if(dto==null){
+			dto=new QueryPageDTO();
+		}
+		Page page=new Page();
+		page.setCurrent(dto.getCurrent());
+		page.setSize(dto.getSize());
+		final QueryPageDTO finalDto = dto;
+		Page page1 = iOscOfferService.getBaseMapper().selectPage(
+				page, Wrappers.<OscOffer>query().lambda()
+						.and(!TextUtils.isEmpty(dto.getKeyword()),
+
+								wrapper1->wrapper1.like(OscOffer::getSchoolDesc, finalDto.getKeyword())
+								.or(
+
+								wrapper -> wrapper
+								. like(OscOffer::getStuName, finalDto.getKeyword())
+								.or(). like(OscOffer::getMajorName, finalDto.getKeyword()
+										)
+								)
+						)
+						//.like(!TextUtils.isEmpty(dto.getKeyword()),OscOffer::getStuName, dto.getKeyword())
+				.orderByDesc(OscOffer::getOfferSort)
+
+		);
+		return R.ok(page1);
+	}
+
+
+	/**
+	 * 根据id查询详情
+	 * @param id id
+	 * @return R
+	 */
+	@ApiOperation(value = "根据id查询详情", notes = "根据id查询详情")
+	@GetMapping("/details/{id}" )
+	public R details(@PathVariable Integer id) {
+		return R.ok(iOscOfferService.getBaseMapper().selectById(id));
+	}
+
+/*
+	*//**
+	 *  获取计划类型列表
+	 *
+	 * @return
+	 *//*
+	@ApiOperation(response = PmPlanType.class, value = " 获取计划类型列表", notes = " 获取计划类型列表")
+	@GetMapping("/list/page")
+	public R listPage(@RequestHeader(name = "TENANT-ID") Integer tenantId , @RequestHeader(name = "Project-Id") Integer projectId  , Page page ) {
+		System.out.println("tenantId====================="+TenantContextHolder.getTenantId());
+		return R.ok(iPmPlanTypeService.page(page ));
+	}*/
+
+
+
+}
